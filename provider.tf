8ee3ad5da4aa4b8a0dcc6140e5992cddac019a54
kangHyekoung <kanghyekoung3694@gmail.com>
2025-06-25 11:44:53 +0900

    init

---
+++ provider.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+  backend "s3" {
+	  bucket        = "backend-bucket-kanghyekoung"
+	  key            = "terraform/state-test/terraform.tfstate"
+	  region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock"
+	}
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
